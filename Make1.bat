6ae12a2708f3f7b43fcafcfe9801e50153ba7088
CamuseCao <946735713camuse@gmail.com>
2019-01-30 21:33:57 +0800

    Add Make.bat
    Add Make.bat
    
    Add Make.bat

---
+++ Make1.bat
@@ -0,0 +1,2 @@
+cd Main
+latexmk --xelatex xmu.tex -file-line-error -halt-on-error -interaction=nonstopmode -synctex=1 %1
---
+++ Make2.bat
@@ -0,0 +1,2 @@
+cd Cover and reconsitution  
+latexmk -pdf -file-line-error -halt-on-error -interaction=nonstopmode -synctex=1 %1
