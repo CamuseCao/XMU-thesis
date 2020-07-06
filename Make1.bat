7e8abb3b4a77e2e9262b341b847fac60ec6559fc
camusecao <camusecao@gmail.com>
2020-07-06 23:04:39 +0800

    modify Make1.bat

--- Make1.bat
+++ Make1.bat
@@ -1,1 +1,1 @@
-latexmk --xelatex xmu.tex -file-line-error -halt-on-error -interaction=nonstopmode -synctex=1 %1
+latexmk -pdfxe xmu.tex -file-line-error -halt-on-error -interaction=nonstopmode -synctex=1 %1
